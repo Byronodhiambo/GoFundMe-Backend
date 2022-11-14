17d062fe01d99e4487cbf3fbbe35498650ff6389
Richie <molunorichie@gmail.com>
2022-11-14 21:00:17 +0100

    feat: signup - auth.controllers.js

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 const config = require('../utils/config')
 const asyncWrapper = require('./../utils/async_wrapper')
-const { CustomAPIError } = require('./../utils/custom_errors')
+const { CustomAPIError, BadRequestError } = require('./../utils/custom_errors')
 
 const User = require('../models/user.models')
 
@@ -13,14 +13,50 @@ const validateEmail = (email) => {
         );
 }
 
-const signin = asyncWrapper(async (req, res, next) => {
+const signup = asyncWrapper(async (req, res, next) => {
+    const { first_name, last_name, email, password } = req.body
+
+    if (!first_name || !last_name || !email || !password) {
+        throw new BadRequestError('Please fill in all fields')
+    } else if (!validateEmail(email)) {
+        throw new BadRequestError('Please enter a valid email address')
+    }
+
+    const existing_user = await User.findOne({ where: { email } })
+    let verification_code, curr_user_email, curr_user_id;
+
+    // No existing user, create new user
+    if (!existing_user) {
+        const user = await User.create({ firstName, lastName, email, password, phone, country, ipi })
+        verification_code = (await getAuthCodes(user.id, 'verification')).verification_code
+        curr_user_email, curr_user_id = user.email, user.id
+    }
+
+    // User already exists, but email, unverified
+    if (!existing_user.isVerified) {
+        verification_code = (await getAuthCodes(existing_user.id, 'verification')).verification_code
+        curr_user_email, curr_user_id = existing_user.email.existing_user.i
+    }
+
+    sendEmail({
+        email: user_email,
+        subject: "Email verification",
+        message: `This is your account verification code ${verification_code}`
+    })
+
+    const { access_token } = await getAuthTokens(curr_user_id)
+
+    return res.status(201).send({
+        message: "Successful, Email verification code sent to users email",
+        access_token
+    })
 })
 
 const login = asyncWrapper(async (req, res, next) => {
 })
 
 module.exports = {
-    signin,
+    signup,
     login
 }
 
